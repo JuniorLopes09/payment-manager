0140db84f6ddf6d12833ce158ed192ec07643ede
Cleonor Junior <cleonorjunior09@gmail.com>
2024-08-24 19:16:46 -0300

    fix: Warning de propriedades não mapeadas

--- src/main/java/com/cleonorjunior/paymentmanager/pagamento/mapper/PagamentoMapper.java
+++ src/main/java/com/cleonorjunior/paymentmanager/pagamento/mapper/PagamentoMapper.java
@@ -4,6 +4,7 @@ import com.cleonorjunior.paymentmanager.pagamento.domain.model.Pagamento;
 import com.cleonorjunior.paymentmanager.pagamento.domain.request.PagamentoRequest;
 import com.cleonorjunior.paymentmanager.pagamento.domain.response.PagamentoResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper( componentModel = "spring")
@@ -11,7 +12,15 @@ public interface PagamentoMapper {
 
     PagamentoResponse mapToResponse(Pagamento pagamento);
 
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "dataInsercao", ignore = true)
+    @Mapping(target = "dataAtualizacao", ignore = true)
+    @Mapping(target = "excluido", ignore = true)
     Pagamento mapToEntity(PagamentoRequest dto);
 
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "dataInsercao", ignore = true)
+    @Mapping(target = "dataAtualizacao", ignore = true)
+    @Mapping(target = "excluido", ignore = true)
     void updateFromDTO(@MappingTarget Pagamento pagamento, PagamentoRequest dto);
 }
