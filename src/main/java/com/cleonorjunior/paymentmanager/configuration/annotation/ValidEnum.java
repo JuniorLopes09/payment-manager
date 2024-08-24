09595aaf15c94c4dc3204c0b25ea170581d07eeb
Cleonor Junior <cleonorjunior09@gmail.com>
2024-08-24 13:37:16 -0300

    feat: Validador de Enums

---
+++ src/main/java/com/cleonorjunior/paymentmanager/configuration/annotation/ValidEnum.java
@@ -0,0 +1,18 @@
+package com.cleonorjunior.paymentmanager.configuration.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnum {
+    String message() default "{default.validEnum.valid.message}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends Enum<?>> enumClass();
+}
