9376d08ac7c7080dd24f8afd5da67c4537b55c82
Cleonor Junior <cleonorjunior09@gmail.com>
2025-02-02 20:02:15 -0300

    feat: Habilitação de segurança por método e hierarquia de roles

--- src/main/java/com/cleonorjunior/paymentmanager/configuration/SecurityConfiguration.java
+++ src/main/java/com/cleonorjunior/paymentmanager/configuration/SecurityConfiguration.java
@@ -3,8 +3,11 @@ package com.cleonorjunior.paymentmanager.configuration;
 import com.cleonorjunior.paymentmanager.configuration.filter.SecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
     private final SecurityFilter securityFilter;
@@ -41,7 +45,6 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
         return configuration.getAuthenticationManager();
-
     }
 
     @Bean
@@ -49,4 +52,10 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    static RoleHierarchy roleHierarchy() {
+        return RoleHierarchyImpl.withDefaultRolePrefix()
+                .role("ADMIN").implies("USUARIO")
+                .build();
+    }
 }
