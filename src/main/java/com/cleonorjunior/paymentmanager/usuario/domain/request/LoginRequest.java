c387917817498713d7b9cec0ffa40b0375515115
Cleonor Junior <cleonorjunior09@gmail.com>
2025-02-02 21:31:38 -0300

    feat: Adição de validações nos request

--- src/main/java/com/cleonorjunior/paymentmanager/usuario/domain/request/LoginRequest.java
+++ src/main/java/com/cleonorjunior/paymentmanager/usuario/domain/request/LoginRequest.java
@@ -1,7 +1,11 @@
 package com.cleonorjunior.paymentmanager.usuario.domain.request;
 
+import com.cleonorjunior.paymentmanager.configuration.annotation.ValidPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
-        String email,
-        String senha
+        @Email @NotBlank String email,
+        @ValidPassword @NotBlank String senha
 ) {
 }
