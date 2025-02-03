a3857a200402b2845c92efefd09db2359324aa8d
Cleonor Junior <cleonorjunior09@gmail.com>
2025-02-02 21:30:14 -0300

    chore: Adição da anotação @NonNull

--- src/main/java/com/cleonorjunior/paymentmanager/configuration/exception/CustomExceptionHandler.java
+++ src/main/java/com/cleonorjunior/paymentmanager/configuration/exception/CustomExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -42,9 +43,9 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatusCode status,
-                                                                  WebRequest request) {
+                                                                  @NonNull HttpHeaders headers,
+                                                                  @NonNull HttpStatusCode status,
+                                                                  @NonNull WebRequest request) {
 
         String uri = ((ServletWebRequest) request).getRequest().getRequestURI();
 
